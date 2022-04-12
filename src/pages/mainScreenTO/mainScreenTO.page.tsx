8a5472a009b8b1f8067a1f5059f9d754e85983fc
MariaFernanda2502 <ferchasalvador25@gmail.com>
2022-04-11 19:18:45 -0500

    v2 Main Screen Tour Operador

--- src/pages/mainScreenTO/mainScreenTO.page.tsx
+++ src/pages/mainScreenTO/mainScreenTO.page.tsx
@@ -25,48 +25,50 @@ function MainScreenTO(props: IMainScreenTO): JSX.Element {
     <ChakraProvider>
       <Container maxWidth="full">
         <Flex h="100vh">
-          <Box bgImage={`url(${fondoMS})`} w="full" h="full" bgSize={"cover"}>
+          <Box bgImage={`url(${fondoMS})`} w="full" h="84%" bgSize={"cover"}>
             <HStack justifyContent="center" w="full" h="full">
-              <Box  w="35%" 
-                    h="65%"
+              <Box  w="38%" 
+                    h="78%"
                     bgColor="white"
-                    p={5}
+                    p={6}
                     borderRadius={20} 
                     marginRight="100" >
-                <VStack alignItems="flex-start">
-                  <HStack>
-                    <VStack alignItems="flex-start" spacing={6}>
-                      <Heading fontSize='30px'> New tour </Heading>
-                      <Text fontSize='25px'> People with disabilities are already booking tours and activities through our platform. Become a certified partner! </Text>
-                    </VStack>
-                    <Image src={ImageInfoMSTO} w="30%"/>
-                  </HStack>
-                  <Text fontSize='25px'> How to become a Wheel the World Partner? </Text>
-                  <HStack>
-                    <Text fontSize='22px'> 1 </Text>
-                    <Text fontSize='22px'> Access to any mobile device or computer </Text>
-                  </HStack>
-                  <HStack>
-                    <Text fontSize='22px'> 2 </Text>
-                    <Text fontSize='22px'> Follow the steps on the app </Text>
-                  </HStack>
-                  <HStack>
-                    <Text fontSize='22px'> 3 </Text>
-                    <Text fontSize='22px'> Our team will take care of the rest </Text>
-                  </HStack>
-                  <HStack justifyContent="center" w="full" p={3}>
-                    <Button size='md'
-                            height='80px'
+                <VStack spacing={8}>
+                  <VStack alignItems="flex-start">
+                    <HStack>
+                      <VStack alignItems="flex-start">
+                        <Heading fontSize='30px'> New tour </Heading>
+                        <Text fontSize='25px'> People with disabilities are already booking tours and activities through our platform. Become a certified partner! </Text>
+                      </VStack>
+                      <Image src={ImageInfoMSTO} w="30%"/>
+                    </HStack>
+                    <Text fontSize='25px'> How to become a Wheel the World Partner? </Text>
+                    <HStack>
+                      <Text fontSize='22px'> 1 </Text>
+                      <Text fontSize='22px'> Access to any mobile device or computer </Text>
+                    </HStack>
+                    <HStack>
+                      <Text fontSize='22px'> 2 </Text>
+                      <Text fontSize='22px'> Follow the steps on the app </Text>
+                    </HStack>
+                    <HStack>
+                      <Text fontSize='22px'> 3 </Text>
+                      <Text fontSize='22px'> Our team will take care of the rest </Text>
+                    </HStack>
+                  </VStack>
+                  <HStack justifyContent="center" w="full">
+                    <Button size='lg'
+                            height='68px'
                             width='200px'
                             fontSize='30px'> 
                       Let's start! </Button>
                   </HStack>
                 </VStack>
               </Box>
-              <Box  w="35%" 
-                    h="65%"
+              <Box  w="38%" 
+                    h="78%"
                     bgColor="white"
-                    p={5}
+                    p={6}
                     borderRadius={20} 
                     marginLeft="100">
                 <Heading marginBottom={5} fontSize='30px'> Tour registered </Heading>
@@ -80,8 +82,8 @@ function MainScreenTO(props: IMainScreenTO): JSX.Element {
                     </Slider>
                     <Text fontSize='22px'> 30% </Text>
                   </HStack>
-                  <Button size='lg'> Continue </Button>
-                  <DeleteIcon w={6} h={6} />
+                  <Button size='lg' fontSize="25px"> Continue </Button>
+                  <DeleteIcon w={8} h={8} />
                 </HStack>
                 <Text fontSize='25px'> Snorkel with whale sharks </Text>
                 <HStack w="full" spacing={6}>
@@ -93,8 +95,8 @@ function MainScreenTO(props: IMainScreenTO): JSX.Element {
                     </Slider>
                     <Text fontSize='22px'> 70% </Text>
                   </HStack>
-                  <Button size='lg'> Continue </Button>
-                  <DeleteIcon w={6} h={6} />
+                  <Button size='lg' fontSize='25px'> Continue </Button>
+                  <DeleteIcon w={8} h={8} />
                 </HStack>
               </Box>
             </HStack>
