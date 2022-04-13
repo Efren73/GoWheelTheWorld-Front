e116500e832aac834309a02e6bea414de48845d4
Veronica Martinez <Vero@MacBook-Pro-de-Salvador.local>
2022-04-13 15:14:31 -0500

    componentes de form V1

---
+++ src/components/GroupPrivate/groupprivate.test.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { screen } from "@testing-library/react"
+import { render } from "../../test-utils"
+import GruoupPrivate from "./groupprivate.component"
+
+test("renders learn react link", () => {
+  render(<GruoupPrivate />)
+})
