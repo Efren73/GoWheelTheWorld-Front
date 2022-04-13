ef0fd78ec921a34d6d42164fdc12aac63e292126
Erik Martinez <72992377+ErikMartinezI@users.noreply.github.com>
2022-04-13 13:16:46 -0500

    Update summary.component.tsx

--- src/components/summary/summary.component.tsx
+++ src/components/summary/summary.component.tsx
@@ -3,142 +3,156 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-  Heading,
-  HStack,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
   Image,
-  AspectRatio,
 } from "@chakra-ui/react"
 
-import photo from './images/photo.png';
-import duration from './images/duration.png';
-import group from './images/group.png';
-import child from './images/child.png';
-import height from './images/height.png';
-import language from './images/language.png';
-import location from './images/location.png';
-import person from './images/person.png';
-import Typetour from './images/type-of-tour.png';
-import price from './images/price.png';
+ 
+function MenuDes() {
+  const {isOpen,onOpen,onClose}= useDisclosure()
+  const btnRef= React.useRef()
+  return (
+    <ChakraProvider>
+      <Box p={4}>
+      <Button colorScheme='blackAlpha' onClick={onOpen}>
+     <Text fontSize='4xl'>≡</Text>
+      </Button>
+        <Drawer 
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        >
+          <DrawerOverlay/>
+          <DrawerContent bg="gray.50" >
+            <DrawerCloseButton/>
+            <DrawerHeader> Send your tour/activity</DrawerHeader>
+            <DrawerBody>
+                <Accordion defaultIndex={[0]} allowMultiple>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                      <AccordionIcon />
+                        <Box flex='1' textAlign='left'>
+                          Basic Information
+                        </Box>
+                       
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                    <Button colorScheme='white' variant='ghost' height='30px' width='250px'>Name</Button>
+                    <br/><Button colorScheme='white' variant='ghost' height='30px' width='250px'>Type</Button>
+                    <br/><Button colorScheme='white' variant='ghost' height='30px' width='250px'>Travelers</Button>
+                    <br/><Button colorScheme='white' variant='ghost' height='30px' width='250px'>Price</Button>
+                    <br/><Button colorScheme='white' variant='ghost' height='30px' width='250px'>Description</Button>
+                    </AccordionPanel>
+                  </AccordionItem>
 
-const Summary = () => (
-  <Box h="full" w={360} background="#000" borderRadius='10px' borderColor={'black'}>
-    <Heading paddingLeft={22} paddingTop={12} color="#fff">
-      Summary
-    </Heading>
-    <Text color='#2F6FE4' paddingLeft={22}>
-      Basic Information
-    </Text>
-    <Box borderRadius='10px' borderColor='#2F6FE4' border='3px solid #2F6FE4' maxW={310} marginLeft={22}>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                      <AccordionIcon />
+                        <Box flex='1' textAlign='left'>
+                          Intinerary
+                        </Box>
+                        
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      <Button colorScheme='white' variant='ghost' height='30px' width='250px'>Meet/End Point</Button>
+                    <br/><Button colorScheme='white' variant='ghost' height='30px' width='250px'>Stops</Button>
+                    <br/><Button colorScheme='white' variant='ghost'height='30px' width='250px'>Spoken Languages</Button>
+                    <br/><Button colorScheme='white' variant='ghost' height='30px' width='250px'>Restriction</Button>
+                    </AccordionPanel>
+                  </AccordionItem>
 
-        <HStack justifyContent='center'>
-          <Image src={photo} alt="default image"  maxWidth={114} maxH={71}/>
-          <Text color='#2F6FE4'>
-            Name of the tour
-          </Text>
-        </HStack>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                      <AccordionIcon />
+                        <Box flex='1' textAlign='left'>
+                          Childern Policy
+                        </Box>
+                        
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                    
+                    </AccordionPanel>
+                  </AccordionItem>
 
-        <HStack justifyContent="en" spacing={6} w="full">
-            <Image src={Typetour} alt="Type of tour icon" w={25} h={25}/>
-          <Text color="#fff">
-            Type of tour
-          </Text> 
-        </HStack>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                      <AccordionIcon />
+                        <Box flex='1' textAlign='left'>
+                          What's Included
+                        </Box>
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      
+                    </AccordionPanel>
+                  </AccordionItem>
 
-        <HStack >
-          <Image src={duration} alt="Duration icon" w={25} h={25} />
-          <Text color="#fff">
-          Duration
-          </Text>
-        </HStack>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                      <AccordionIcon />
+                        <Box flex='1' textAlign='left'>
+                          Accessibility
+                        </Box>
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={5}>
+                    <Button colorScheme='white' variant='ghost' height='30px' width='250px'>Assistance‏‏</Button>
+                    <br/><Button colorScheme='white' variant='ghost' height='30px' width='250px'>Transportation‏‏‏‏‎‎</Button>
+                    <br/><Button colorScheme='white' variant='ghost' height='30px' width='250px'>Restrooms</Button>
+                    <br/><Button colorScheme='white' variant='ghost'height='30px' width='250px'>Places</Button>
+                    <br/><Button colorScheme='white' variant='ghost' height='30px' width='250px'>Equipment</Button>
+                    </AccordionPanel>
+                  </AccordionItem>
+                  
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                      <AccordionIcon />
+                        <Box flex='1' textAlign='left'>
+                          FAQ
+                        </Box>
+                        
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                     
+                    </AccordionPanel>
+                  </AccordionItem>
 
-        <HStack >
-          <Image src={group} alt="Group icon" w={25} h={25} />
-          <Text color="#fff">
-            Group/Private tour
-          </Text>
-        </HStack>
+                </Accordion>
 
-        <HStack >
-          <Image src={person} alt="Number of members icon" w={25} h={25} />
-          <Text color="#fff">
-            Number of traveleres
-          </Text>
-        </HStack>
+            </DrawerBody>
 
-        <HStack >
-        <Image src={price} alt="Price icon" w={25} h={25} />
-          <Text color="#fff">
-            $ Price
-          </Text>
-        </HStack>
-    </Box>
+            
+          </DrawerContent>
+        </Drawer>
 
-    <Text color='#2F6FE4' paddingLeft={22}>
-      Itinerary
-    </Text>
 
-    <Box borderRadius='10px' borderColor='#2F6FE4' border='3px solid #2F6FE4' maxW={310} marginLeft={22}>
-        <HStack justifyContent="en" spacing={6} w="full">
-            <Image src={location} alt="Meeting point icon" w={25} h={25}/>
-          <Text color="#fff">
-            Meeting point
-          </Text> 
-        </HStack>
+      </Box>
+    </ChakraProvider>
+ 
+      
+  )
+}
 
-        <HStack >
-          <Image src={location} alt="End point icon" w={25} h={25} />
-          <Text color="#fff">
-            End point
-          </Text>
-        </HStack>
-        <HStack >
-          <Image src={language} alt="Language icon" w={25} h={25} />
-          <Text color="#fff">
-            Language
-          </Text>
-        </HStack>
-    </Box>
-
-    <Text color='#2F6FE4' paddingLeft={22}>
-      Children Policy
-    </Text>
-
-    <Box borderRadius='10px' borderColor='#2F6FE4' border='3px solid #2F6FE4' maxW={310} marginLeft={22}>
-        <HStack justifyContent="en" spacing={6} w="full">
-            <Image src={child} alt="Child icon" w={25} h={25}/>
-          <Text color="#fff">
-          Children's allow age
-          </Text> 
-        </HStack>
-
-        <HStack >
-          <Image src={price} alt="Price icon" w={25} h={25} />
-          <Text color="#fff">
-          Children's pay from age
-          </Text>
-        </HStack>
-        <HStack >
-          <Image src={height} alt="Height icon" w={25} h={25} />
-          <Text color="#fff">
-            Limit height
-          </Text>
-        </HStack>
-    </Box>
-
-    <Text color='#2F6FE4' paddingLeft={22}>
-      What's included?
-    </Text>
-
-    <Box borderRadius='10px' borderColor='#2F6FE4' border='3px solid #2F6FE4' maxW={310} marginLeft={22}>
-      <Text color="#fff">
-        Here are the things included on this tour
-      </Text>
-    </Box>
-  </Box>
-) 
-export default Summary;
+export default MenuDes;
