df4a1032258999e57dbc1cb0cd0d45c63162ee6d
Erik Martinez <72992377+ErikMartinezI@users.noreply.github.com>
2022-04-13 13:17:48 -0500

    Update summary.test.tsx

--- src/components/summary/summary.test.tsx
+++ src/components/summary/summary.test.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { screen } from "@testing-library/react"
-import { render } from "../../test-utils"
-import Summary from "."
+import { render } from "../../test-utils";
+import MenuDes from ".";
 
-test("renders learn react link", () => {
-  render(<Summary />)
+test("menu component", () => {
+  render(<MenuDes />)
 })
