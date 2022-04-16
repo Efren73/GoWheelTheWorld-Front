438486fa14119134c6157108c827ae39ee918b70
Efren73 <A01276266@itesm.mx>
2022-04-15 21:38:27 -0500

    Agregando algunos exports faltantes al index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
 export { default as Summary } from './summary/summary.component';
 export { default as Cart } from './Cart/cart.component';
+export { default as GruoupPrivate } from './GroupPrivate/groupprivate.component';
+export { default as Multiple } from './Multiple/multiple.component';
+export { default as Price } from './Price/price.component';
+export { default as LateralMenu } from './Lateral-Menu/lateral-menu.component';
