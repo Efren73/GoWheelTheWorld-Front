7f66b8f13b8ca988037258e3609060e8cc1f357e
Efren73 <A01276266@itesm.mx>
2022-05-03 17:26:23 -0500

    Funcionalidad de equipment, places, restroom y transportation

--- src/components/Transportation/transportation.component.tsx
+++ src/components/Transportation/transportation.component.tsx
@@ -14,8 +14,36 @@ import {
   Radio
 } from "@chakra-ui/react"
 import { ITransportation } from "./transportation.types";
+import {useState} from 'react'
 
 function Transportation(props: ITransportation): JSX.Element {
+
+    const [transport, setTransport] = useState<any>([
+    {
+        question: "Transportation is wheelchair accessible",
+        answer: ''
+    },
+    {
+        question: "Transportation is not wheelchair accessible but assistance will be provided for transfers.",
+        answer: ''
+    },
+    {
+        question: "If needed, your wheelchair can be stored safely inside the vehicle",
+        answer: ''
+    },
+    {
+        question: "Is transportation included in this tour/activity?",
+        answer: ''
+    }])
+
+    console.log(transport)
+
+    function changeValue(e: any, index: any){
+        let newArray: any[] = [...transport];
+        newArray[index].answer = e.target.value;
+        setTransport(newArray)
+    }
+
     return(
         <Box boxShadow='2xl'
             w="65%" 
@@ -28,7 +56,7 @@ function Transportation(props: ITransportation): JSX.Element {
                 <Text fontSize='20px' color='#3F6FE4'> Accesibility / Transportation </Text>
                 <Heading fontSize='35px'>Transportation</Heading>
 
-                <HStack justifyContent="flex-end" w="95%">
+                <HStack justifyContent="flex-end" w="93%">
                     <HStack w="15%" spacing={31}>
                         <Text color="#4F6FE4"> Yes </Text>
                         <Text color="#4F6FE4"> No </Text>
@@ -40,14 +68,14 @@ function Transportation(props: ITransportation): JSX.Element {
                             <Tr>
                                 <Td >Transportation is wheelchair accessible</Td>
                                 <Td>
-                                    <RadioGroup>
+                                    <RadioGroup value = {transport[0].answer} >
                                         <HStack spacing={8} justifyContent='flex-end'>
                                             <Radio  value='yes'
                                                     border='1px'
-                                                    borderColor='#2F6FE4'></Radio>
+                                                    borderColor='#2F6FE4' onChange={(e) => changeValue(e, 0)}></Radio>
                                             <Radio  value='no'
                                                     border='1px'
-                                                    borderColor='#2F6FE4'></Radio>
+                                                    borderColor='#2F6FE4' onChange={(e) => changeValue(e, 0)}></Radio>
                                         </HStack>
                                     </RadioGroup>
                                 </Td>
@@ -58,14 +86,16 @@ function Transportation(props: ITransportation): JSX.Element {
                                     provided for transfers.
                                 </Td>
                                 <Td>
-                                    <RadioGroup>
+                                    <RadioGroup value = {transport[1].answer}>
                                         <HStack spacing={8} justifyContent='flex-end'>
                                             <Radio  value='yes'
                                                     border='1px'
-                                                    borderColor='#2F6FE4'></Radio>
+                                                    borderColor='#2F6FE4'
+                                                    onChange={(e) => changeValue(e, 1)}></Radio>
                                             <Radio  value='no'
                                                     border='1px'
-                                                    borderColor='#2F6FE4'></Radio>
+                                                    borderColor='#2F6FE4'
+                                                    onChange={(e) => changeValue(e, 1)}></Radio>
                                         </HStack>
                                     </RadioGroup>
                                 </Td>
@@ -73,14 +103,16 @@ function Transportation(props: ITransportation): JSX.Element {
                             <Tr>
                                 <Td >If needed, your wheelchair can be stored safely inside the vehicle</Td>
                                 <Td>
-                                    <RadioGroup>
+                                    <RadioGroup value = {transport[2].answer}>
                                         <HStack spacing={8} justifyContent='flex-end'>
                                             <Radio  value='yes'
                                                     border='1px'
-                                                    borderColor='#2F6FE4'></Radio>
+                                                    borderColor='#2F6FE4'
+                                                    onChange={(e) => changeValue(e, 2)}></Radio>
                                             <Radio  value='no'
                                                     border='1px'
-                                                    borderColor='#2F6FE4'></Radio>
+                                                    borderColor='#2F6FE4'
+                                                    onChange={(e) => changeValue(e, 2)}></Radio>
                                         </HStack>
                                     </RadioGroup>
                                 </Td>
@@ -88,14 +120,16 @@ function Transportation(props: ITransportation): JSX.Element {
                             <Tr>
                                 <Td>Is transportation included in this tour/activity?</Td>
                                 <Td>
-                                    <RadioGroup>
+                                    <RadioGroup value = {transport[3].answer}>
                                         <HStack spacing={8} justifyContent='flex-end'>
                                             <Radio  value='yes'
                                                     border='1px'
-                                                    borderColor='#2F6FE4'></Radio>
+                                                    borderColor='#2F6FE4'
+                                                    onChange={(e) => changeValue(e, 3)}></Radio>
                                             <Radio  value='no'
                                                     border='1px'
-                                                    borderColor='#2F6FE4'></Radio>
+                                                    borderColor='#2F6FE4'
+                                                    onChange={(e) => changeValue(e, 3)}></Radio>
                                         </HStack>
                                     </RadioGroup>
                                 </Td>
