b467d49167a8e544b0c0e2f569ead9da1e116e95
Carlos Manuel Gonzalez Vallejo <A01276000@tec.mx>
2022-05-02 23:16:23 -0500

    Pantalla de Question Responsive

--- src/pages/question/Header/header.page.tsx
+++ src/pages/question/Header/header.page.tsx
@@ -2,26 +2,75 @@ import * as React from "react"
 
 import {
     Text,
-    VStack,
-    Box,
+    Button,
+    useDisclosure,
+    useBreakpointValue,
     HStack,
+    Box,
+    Drawer,
+    VStack,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
     Link,
     Image,
   } from "@chakra-ui/react"
-  
+
+  import {HamburgerIcon } from '@chakra-ui/icons'
+  import Summary from "../../../components/summary"
   import logo from '../../login/images/logo.png'
 
-  const Header = () => (
-    <HStack w='full' h="16%" spacing={'70%'}>
-        <Box alignItems={'flex-start'} w="10%">
+  const Header = () =>{ 
+
+    const screenSize = useBreakpointValue({ base: 'true', md: 'false', lg:'false' })
+
+
+    function SummaryDrawer() {
+        const { isOpen, onOpen, onClose } = useDisclosure()
+      
+        return (
+          <>
+            <Button colorScheme='gray' leftIcon={<HamburgerIcon />}  onClick={onOpen}>
+                Summary
+            </Button>
+
+            <Drawer
+              isOpen={isOpen}
+              placement='right'
+              onClose={onClose}
+              size={'full'}
+            >
+              <DrawerOverlay />
+              <DrawerContent w='full'>
+              <DrawerCloseButton color='white'/>
+                <Summary/>
+              </DrawerContent>
+            </Drawer>
+          </>
+        )
+      }
+      function CheckSize(screenSize:any){
+        if (screenSize=='true')
+            return (
+                <SummaryDrawer/>
+                )
+    }
+
+    return (
+    <HStack w='full'  h={20} justifyContent='space-between' paddingRight='10%'>
+        <Box paddingLeft={{sm:'5'}} w={{base: '10%', md: '15%', lg:'10%', sm: '30%'}}>
             <Image src={logo} />
         </Box>
         <Box alignItems={'flex-end'}>
             <VStack alignItems="flex-start" spacing="-2">
-                <Text fontSize="30px" color="#3F6FE4"> Fernanda, let's start! </Text>
-                <Link fontSize="25px"> Save and exit </Link>
+                <Text fontSize={['10px', '15px', '20px', '25px']} color="#3F6FE4"> Fernanda, let's start! </Text>
+                <Link fontSize={['10px', '10px', '15px', '20px']} > Save and exit </Link>
             </VStack>
         </Box>
+        { 
+            CheckSize(screenSize)
+         }
     </HStack>
-  )
+
+  )}
   export default Header;
